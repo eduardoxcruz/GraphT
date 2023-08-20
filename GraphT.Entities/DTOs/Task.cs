f38e3c58a1f6eac454efc715345c73386df6293f
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-08-20 02:36:58 -0600

    Set properties as nullables

--- GraphT.Entities/DTOs/Task.cs
+++ GraphT.Entities/DTOs/Task.cs
@@ -12,23 +12,23 @@ public class Task
     
     #region Punctuality And Time Spend
 
-    public DateTime StartDate { get; set; }
-    public DateTime FinishDate { get; set; }
-    public DateTime LimitDate { get; set; }
-    public TimeSpan TimeSpend { get; set; }
-    public string Punctuality { get; set; }
     public DateTime CreatedTime { get; init; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? FinishDate { get; set; }
+    public DateTime? LimitDate { get; set; }
+    public TimeSpan? TimeSpend { get; set; }
+    public string? Punctuality { get; set; }
 
     #endregion
 
     #region Rewards
 
-    public decimal ExpReward { get; set; }
-    public decimal GoldReward { get; set; }
+    public decimal? ExpReward { get; set; }
+    public decimal? GoldReward { get; set; }
 
     #endregion
     
-    public List<Task> Upstreams { get; set; }
-    public List<Task> Downstreams { get; set; }
-    public List<Area> Areas { get; set; }
+    public List<Task>? Upstreams { get; set; }
+    public List<Task>? Downstreams { get; set; }
+    public List<Area>? Areas { get; set; }
 }
