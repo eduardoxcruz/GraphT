e5b3f15987dd5795001a7d3caeb3b1c04d9d0642
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-08-23 21:05:50 -0600

    Add FromValues_ReturnsPurposeful_WhenIsFunAndIsProductive() in PriorityTests

--- Model.Tests/PriorityTests.cs
+++ Model.Tests/PriorityTests.cs
@@ -1,6 +1,24 @@
-﻿namespace Model.Tests;
+﻿using Model.ValueObjects;
+
+namespace Model.Tests;
 
 public class PriorityTests
 {
-    
+    [Fact]
+    public void FromValues_ReturnsPurposeful_WhenIsFunAndIsProductive()
+    {
+        Priority priority;
+        bool isFun;
+        bool isProductive;
+
+        // Arrange
+        isFun = true;
+        isProductive = true;
+
+        // Act
+        priority = Priority.FromValues(isFun, isProductive);
+
+        // Assert
+        Assert.Equal(Priority.Purposeful, priority);
+    }
 }
