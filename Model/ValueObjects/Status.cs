8b5c0accc4fb7c19f413ca4504a46a42068fc4a1
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-08-23 21:14:37 -0600

    Add properties to Status

--- Model/ValueObjects/Status.cs
+++ Model/ValueObjects/Status.cs
@@ -4,5 +4,12 @@ namespace Model.ValueObjects;
 
 public class Status : Enumeration
 {
+    public static Status Completed = new(1, nameof(Completed));
+    public static Status Droped = new(2, nameof(Droped));
+    public static Status Paused = new(3, nameof(Paused));
+    public static Status InProgress = new(4, nameof(InProgress));
+    public static Status ReadyToStart = new(5, nameof(ReadyToStart));
+    public static Status Backlog = new(6, nameof(Backlog));
+    
     public Status(int id, string name) : base(id, name) { }
 }
