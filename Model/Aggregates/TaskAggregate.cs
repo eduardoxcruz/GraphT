da2d405f348dad451ecda6efdbfd24a2cfbd9170
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-08-23 21:21:05 -0600

    Add TodoTask in TaskAggregate

--- Model/Aggregates/TaskAggregate.cs
+++ Model/Aggregates/TaskAggregate.cs
@@ -1,11 +1,21 @@
-﻿using Model.ValueObjects;
+﻿using Model.Entities;
+using Model.ValueObjects;
 
 namespace Model.Aggregates;
 
-public class TaskAggregate
+public class TaskAggregate : TodoTask
 {
     public bool IsFun { get; private set; }
     public bool IsProductive { get; private set; }
     public Priority Priority { get; private set; }
     public Status Status { get; private set; }
+
+    public TaskAggregate(long id, string name, bool isFun, bool isProductive, Status status) 
+        : base(id, name)
+    {
+        IsFun = isFun;
+        IsProductive = isProductive;
+        Priority = Priority.FromValues(IsFun, IsProductive);
+        Status = status;
+    }
 }
