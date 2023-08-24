359094dfdec6ff062586a307d5fcdfbeecd0501c
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-08-23 20:38:35 -0600

    Change IsFun and IsProductive properties "set" accessor to private in TodoTask

--- Model/Entities/TodoTask.cs
+++ Model/Entities/TodoTask.cs
@@ -4,8 +4,8 @@ public class TodoTask
 {
     public long Id { get; set; }
     public string Name { get; set; }
-    public bool IsFun { get; set; }
-    public bool IsProductive { get; set; }
+    public bool IsFun { get; private set; }
+    public bool IsProductive { get; private set; }
     public float Progress { get; set; }
 
     public TodoTask(long id, string name, bool isFun, bool isProductive, float progress)
