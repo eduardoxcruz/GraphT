9e43803290cea3e5cc69fcde342c19889a0c08b9
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-08-23 21:20:33 -0600

    Add IsFun and IsProductive properties in TaskAggregate

--- Model/Aggregates/TaskAggregate.cs
+++ Model/Aggregates/TaskAggregate.cs
@@ -4,6 +4,8 @@ namespace Model.Aggregates;
 
 public class TaskAggregate
 {
+    public bool IsFun { get; private set; }
+    public bool IsProductive { get; private set; }
     public Priority Priority { get; private set; }
     public Status Status { get; private set; }
 }
