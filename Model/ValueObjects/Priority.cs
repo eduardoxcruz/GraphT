effe039bbe0fb10e7ce856469001b59f249b5f93
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-08-23 20:52:38 -0600

    Add public properties to Priority

--- Model/ValueObjects/Priority.cs
+++ Model/ValueObjects/Priority.cs
@@ -4,6 +4,11 @@ namespace Model.ValueObjects;
 
 public class Priority : Enumeration
 {
+    public static Priority Purposeful = new(1, nameof(Purposeful));
+    public static Priority Necessary = new(2, nameof(Necessary));
+    public static Priority Entertaining = new(3, nameof(Entertaining));
+    public static Priority Superfluous = new(4, nameof(Superfluous));
+    
     public Priority(int id, string name) : base(id, name)
     {
     }
