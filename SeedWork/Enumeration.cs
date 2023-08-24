ae9b729f81566a8d9b6d3a7f4c83ec68fc8d968f
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-08-23 20:43:20 -0600

    Add public properties in Enumeration

--- SeedWork/Enumeration.cs
+++ SeedWork/Enumeration.cs
@@ -2,6 +2,10 @@
 
 public abstract class Enumeration : IComparable
 {
+    public string Name { get; private set; }
+
+    public int Id { get; private set; }
+    
     public int CompareTo(object? obj)
     {
         throw new NotImplementedException();
