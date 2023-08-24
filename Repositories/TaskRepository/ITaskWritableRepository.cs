832fc3f36dff0487f0bb4455d22dac5a9e233250
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-08-23 23:29:29 -0600

    Add Create(TaskAggregate) in ITaskWritableRepository

--- Repositories/TaskRepository/ITaskWritableRepository.cs
+++ Repositories/TaskRepository/ITaskWritableRepository.cs
@@ -1,6 +1,8 @@
-﻿namespace Repositories.TaskRepository;
+﻿using Model.Aggregates;
+
+namespace Repositories.TaskRepository;
 
 public interface ITaskWritableRepository
 {
-    
+    void Create(TaskAggregate task);
 }
