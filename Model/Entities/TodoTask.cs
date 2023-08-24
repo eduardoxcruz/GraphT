659b5ac76031432c8cc9f3b782aad54edbc98529
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-08-23 20:18:31 -0600

    Add properties in TodoTask

--- Model/Entities/TodoTask.cs
+++ Model/Entities/TodoTask.cs
@@ -2,5 +2,9 @@
 
 public class TodoTask
 {
-    
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public bool IsFun { get; set; }
+    public bool IsProductive { get; set; }
+    public float Progress { get; set; }
 }
