62f303fbf811491b0da91a158947691f4b8be5a1
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-08-23 20:50:47 -0600

    Add Enumeration in Priority

--- Model/ValueObjects/Priority.cs
+++ Model/ValueObjects/Priority.cs
@@ -1,6 +1,10 @@
-﻿namespace Model.ValueObjects;
+﻿using SeedWork;
 
-public class Priority
+namespace Model.ValueObjects;
+
+public class Priority : Enumeration
 {
-    
+    public Priority(int id, string name) : base(id, name)
+    {
+    }
 }
