d304e9505005a7f7e4f44ab1c017a9b330bd38fa
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-08-23 20:18:53 -0600

    Add constructor in TodoTask

--- Model/Entities/TodoTask.cs
+++ Model/Entities/TodoTask.cs
@@ -7,4 +7,13 @@ public class TodoTask
     public bool IsFun { get; set; }
     public bool IsProductive { get; set; }
     public float Progress { get; set; }
+
+    public TodoTask(long id, string name, bool isFun, bool isProductive, float progress)
+    {
+        Id = id;
+        Name = name;
+        IsFun = isFun;
+        IsProductive = isProductive;
+        Progress = progress;
+    }
 }
