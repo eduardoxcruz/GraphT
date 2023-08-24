bed82b52d27d4ebdae6871e1cba9cdbf098ffd09
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-08-23 23:38:43 -0600

    Add CreateIsolatedTask(CreateIsolatedTaskInputDto) in ICreateIsolatedTaskController

--- Controllers/CreateIsolatedTask/ICreateIsolatedTaskController.cs
+++ Controllers/CreateIsolatedTask/ICreateIsolatedTaskController.cs
@@ -1,6 +1,8 @@
-﻿namespace Controllers.CreateIsolatedTask;
+﻿using DTOs.CreateIsolatedTask;
+
+namespace Controllers.CreateIsolatedTask;
 
 public interface ICreateIsolatedTaskController
 {
-    
+    ValueTask<CreateIsolatedTaskOutputDto> CreateIsolatedTask(CreateIsolatedTaskInputDto dto);
 }
