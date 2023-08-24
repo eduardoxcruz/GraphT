b5918802b45d1d4d5c2efc098b3e58602cc1dfff
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-08-23 23:50:51 -0600

    Add properties in CreateIsolatedTaskInputDto

--- DTOs/CreateIsolatedTask/CreateIsolatedTaskInputDto.cs
+++ DTOs/CreateIsolatedTask/CreateIsolatedTaskInputDto.cs
@@ -1,6 +1,11 @@
-﻿namespace DTOs.CreateIsolatedTask;
+﻿using Model.ValueObjects;
+
+namespace DTOs.CreateIsolatedTask;
 
 public class CreateIsolatedTaskInputDto
 {
-    
+    public string Name { get; set; }
+    public bool IsFun { get; set; }
+    public bool IsProductive { get; set; }
+    public string Status { get;  set; }
 }
