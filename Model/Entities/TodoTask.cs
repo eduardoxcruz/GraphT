660efcb32cf518e91a984dd36cf30ec14108539b
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-08-23 21:17:58 -0600

    Delete IsFun, IsProductive and Progress properties in TodoTask

--- Model/Entities/TodoTask.cs
+++ Model/Entities/TodoTask.cs
@@ -4,16 +4,10 @@ public class TodoTask
 {
     public long Id { get; set; }
     public string Name { get; set; }
-    public bool IsFun { get; private set; }
-    public bool IsProductive { get; private set; }
-    public float Progress { get; private set; }
 
-    public TodoTask(long id, string name, bool isFun, bool isProductive, float progress)
+    public TodoTask(long id, string name, bool isFun, bool isProductive)
     {
         Id = id;
         Name = name;
-        IsFun = isFun;
-        IsProductive = isProductive;
-        Progress = progress;
     }
 }
