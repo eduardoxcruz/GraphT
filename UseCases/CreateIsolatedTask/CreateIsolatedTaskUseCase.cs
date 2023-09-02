0883f92bca6ef9ee7ba05a7f03182cc398a85a01
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-09-02 14:48:19 -0600

    Add private readonly properties in CreateIsolatedTaskUseCase

--- UseCases/CreateIsolatedTask/CreateIsolatedTaskUseCase.cs
+++ UseCases/CreateIsolatedTask/CreateIsolatedTaskUseCase.cs
@@ -1,6 +1,11 @@
-﻿namespace UseCases.CreateIsolatedTask;
+﻿using Common.Repositories.TaskRepository;
+
+using UseCasesPorts.CreateIsolatedTask;
+
+namespace UseCases.CreateIsolatedTask;
 
 public class CreateIsolatedTaskUseCase
 {
-    
+    private readonly ICreateIsolatedTaskOutputPort _outputPort;
+    private readonly ITaskWritableRepository _taskRepository;
 }
