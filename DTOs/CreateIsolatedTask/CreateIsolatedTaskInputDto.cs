629b7267c83d4d9c28b47d1ecd901d9a65189956
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-09-02 15:07:12 -0600

    Replace string type to Status type in Status propertie in CreateIsolatedTaskInputDto.cs

--- DTOs/CreateIsolatedTask/CreateIsolatedTaskInputDto.cs
+++ DTOs/CreateIsolatedTask/CreateIsolatedTaskInputDto.cs
@@ -7,5 +7,5 @@ public class CreateIsolatedTaskInputDto
     public string Name { get; set; }
     public bool IsFun { get; set; }
     public bool IsProductive { get; set; }
-    public string Status { get;  set; }
+    public Status Status { get;  set; }
 }
