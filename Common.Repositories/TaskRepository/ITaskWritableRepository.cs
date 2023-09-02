a5600661c96a2428b5c76004faed3aaf4b96b879
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-09-02 15:15:55 -0600

    Add Create(TodoTask) in ITaskWritableRepository

--- Common.Repositories/TaskRepository/ITaskWritableRepository.cs
+++ Common.Repositories/TaskRepository/ITaskWritableRepository.cs
@@ -1,8 +1,11 @@
 ﻿using Model.Aggregates;
+using Model.Entities;
 
 namespace Common.Repositories.TaskRepository;
 
 public interface ITaskWritableRepository
 {
+    void Create(TodoTask task);
+    
     void Create(TaskAggregate task);
 }
