d25cb79fed53f2d5426835a250309cdaea4d7bc0
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-09-21 21:20:14 -0600

    Add constructor in CreateIsolatedTaskController

--- Controllers/CreateIsolatedTask/CreateIsolatedTaskController.cs
+++ Controllers/CreateIsolatedTask/CreateIsolatedTaskController.cs
@@ -9,7 +9,12 @@ public class CreateIsolatedTaskController : ICreateIsolatedTaskController
     private readonly ICreateIsolatedTaskInputPort _inputPort;
     private readonly ICreateIsolatedTaskOutputPort _outputPort;
 
-    
+    public CreateIsolatedTaskController(ICreateIsolatedTaskInputPort inputPort, ICreateIsolatedTaskOutputPort outputPort)
+    {
+        _inputPort = inputPort;
+        _outputPort = outputPort;
+    }
+
     public ValueTask<CreateIsolatedTaskOutputDto> CreateIsolatedTask(CreateIsolatedTaskInputDto dto)
     {
         throw new NotImplementedException();
