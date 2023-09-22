b54bf5f89d8ec2d8049b95271e2f301e62ae2106
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-09-21 21:17:11 -0600

    Revert "Delete Controllers project"
    This reverts commit a47d94076bd9f5369149c6f5fd2506ce6151112e.

---
+++ Controllers/CreateIsolatedTask/CreateIsolatedTaskController.cs
@@ -0,0 +1,11 @@
+﻿using DTOs.CreateIsolatedTask;
+
+namespace Controllers.CreateIsolatedTask;
+
+public class CreateIsolatedTaskController : ICreateIsolatedTaskController
+{
+    public ValueTask<CreateIsolatedTaskOutputDto> CreateIsolatedTask(CreateIsolatedTaskInputDto dto)
+    {
+        throw new NotImplementedException();
+    }
+}
