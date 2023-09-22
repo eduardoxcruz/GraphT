141f3e91713391074122e8ffa05a9ed30c645199
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-09-21 21:19:49 -0600

    Add private properties in CreateIsolatedTaskController

--- Controllers/CreateIsolatedTask/CreateIsolatedTaskController.cs
+++ Controllers/CreateIsolatedTask/CreateIsolatedTaskController.cs
@@ -1,9 +1,15 @@
 ﻿using DTOs.CreateIsolatedTask;
 
+using UseCasesPorts.CreateIsolatedTask;
+
 namespace Controllers.CreateIsolatedTask;
 
 public class CreateIsolatedTaskController : ICreateIsolatedTaskController
 {
+    private readonly ICreateIsolatedTaskInputPort _inputPort;
+    private readonly ICreateIsolatedTaskOutputPort _outputPort;
+
+    
     public ValueTask<CreateIsolatedTaskOutputDto> CreateIsolatedTask(CreateIsolatedTaskInputDto dto)
     {
         throw new NotImplementedException();
