756d0b39938f4793b1298391e47a414143be0eb4
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-09-25 21:54:33 -0600

    Add IUnitOfWork inheritance in UnitOfWork

--- Neo4j.Repositories/UnitOfWork.cs
+++ Neo4j.Repositories/UnitOfWork.cs
@@ -1,13 +1,20 @@
-﻿using Neo4jClient;
+﻿using Common.Interfaces;
+
+using Neo4jClient;
 
 namespace Neo4j.Repositories;
 
-public class UnitOfWork
+public class UnitOfWork : IUnitOfWork
 {
     private readonly IGraphClient _graphClient;
 
     public UnitOfWork(IGraphClient graphClient)
     {
         _graphClient = graphClient;
+    }
+
+    public ValueTask<int> SaveChanges()
+    {
+        throw new NotImplementedException();
     }
 }
