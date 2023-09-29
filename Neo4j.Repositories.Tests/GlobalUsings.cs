dd4b01df8c089d44f5753156917da449d6091b52
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-09-28 23:42:44 -0600

    Add usings in Neo4j.Repositories.Tests.GlobalUsings

--- Neo4j.Repositories.Tests/GlobalUsings.cs
+++ Neo4j.Repositories.Tests/GlobalUsings.cs
@@ -1,1 +1,10 @@
 global using Xunit;
+
+global using Model.Entities;
+global using Model.ValueObjects;
+
+global using Neo4j.Repositories.TaskRepository;
+
+global using Neo4jClient;
+
+global using NSubstitute;
