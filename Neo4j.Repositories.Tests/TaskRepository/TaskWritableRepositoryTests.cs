dd786836a388428f8e05af3f57b304bcea34935f
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-09-28 23:42:57 -0600

    Add Create_ShouldCreateTaskAndReturnId() in TaskWritableRepositoryTests

--- Neo4j.Repositories.Tests/TaskRepository/TaskWritableRepositoryTests.cs
+++ Neo4j.Repositories.Tests/TaskRepository/TaskWritableRepositoryTests.cs
@@ -2,5 +2,23 @@
 
 public class TaskWritableRepositoryTests
 {
-    
+    [Fact]
+    public async void Create_ShouldCreateTaskAndReturnId()
+    {
+        // Arrange
+        IGraphClient graphClient = Substitute.For<IGraphClient>();
+        TaskWritableRepository repository = new(graphClient);
+        TodoTask task = new("Test task", true, false, Status.ReadyToStart);
+
+        // Act
+        var taskId = await repository.Create(task);
+
+        // Assert
+        await graphClient.Cypher
+            .Received(1)
+            .Create("(todoTask:TodoTask {todoTask})")
+            .WithParam("todoTask", task)
+            .ExecuteWithoutResultsAsync();
+        Assert.Equal(task.Id, taskId);
+    }
 }
