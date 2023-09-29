3c697d349c185a91664875ff9b57d5627bb0f359
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-09-28 22:22:24 -0600

    Refactor Handle_ShouldCreateTaskAndCallOutputPort() in CreateIsolatedTaskUseCaseTests

--- UseCases.Tests/CreateIsolatedTask/CreateIsolatedTaskUseCaseTests.cs
+++ UseCases.Tests/CreateIsolatedTask/CreateIsolatedTaskUseCaseTests.cs
@@ -1,5 +1,4 @@
-﻿using Common.Interfaces;
-using Common.Repositories.TaskRepository;
+﻿using Common.Repositories.TaskRepository;
 
 using DTOs.CreateIsolatedTask;
 
@@ -17,34 +16,33 @@ namespace UseCases.Tests.CreateIsolatedTask;
 public class CreateIsolatedTaskUseCaseTests
 {
     [Fact]
-    public async void Handle_ShouldCreateNewTaskAndCallOutputPort()
+    public async void Handle_ShouldCreateTaskAndCallOutputPort()
     {
         // Arrange
+        ICreateIsolatedTaskOutputPort outputPort = Substitute.For<ICreateIsolatedTaskOutputPort>();
+        ITaskWritableRepository taskRepository = Substitute.For<ITaskWritableRepository>();
+        CreateIsolatedTaskUseCase useCase = new(outputPort, taskRepository);
         CreateIsolatedTaskInputDto inputDto = new()
         {
             Name = "Test task", 
             IsFun = true, 
             IsProductive = false, 
-            Status = Status.InProgress
+            Status = Status.ReadyToStart
         };
-        ICreateIsolatedTaskOutputPort? outputPort = Substitute.For<ICreateIsolatedTaskOutputPort>();
-        ITaskWritableRepository? taskRepository = Substitute.For<ITaskWritableRepository>();
-        IUnitOfWork? unitOfWork = Substitute.For<IUnitOfWork>();
-        CreateIsolatedTaskUseCase useCase = new(outputPort, taskRepository, unitOfWork);
 
         // Act
+        taskRepository
+            .Create(Arg.Any<TodoTask>())
+            .Returns("New Task Id");
         await useCase.Handle(inputDto);
 
         // Assert
-        TodoTask expectedTask = new(inputDto.Name, inputDto.IsFun, inputDto.IsProductive, inputDto.Status);
-        taskRepository.Received(1).Create(Arg.Is<TodoTask>(todoTask => 
-            todoTask.Name.Equals(expectedTask.Name) &&
-            todoTask.IsFun == expectedTask.IsFun &&
-            todoTask.IsProductive == expectedTask.IsProductive &&
-            todoTask.Status.Equals(expectedTask.Status) &&
-            todoTask.Priority.Equals(expectedTask.Priority)
-            ));
-        await unitOfWork.Received(1).SaveChanges();
-        await outputPort.Received(1).Handle(Arg.Is<CreateIsolatedTaskOutputDto>(o => !String.IsNullOrEmpty(o.TaskId)));
+        await taskRepository.Received(1).Create(Arg.Is<TodoTask>(todoTask => 
+                todoTask.Name == inputDto.Name && 
+                todoTask.IsFun == inputDto.IsFun && 
+                todoTask.IsProductive == inputDto.IsProductive && 
+                todoTask.Status.Equals(inputDto.Status)));
+        await outputPort.Received(1).Handle(Arg.Is<CreateIsolatedTaskOutputDto>(outputDto => 
+            outputDto.TaskId!.Equals("New Task Id")));
     }
 }
