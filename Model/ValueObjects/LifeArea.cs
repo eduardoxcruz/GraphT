29bc929fdd433e801de32a658c322dd3a13eae12
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-10-04 21:59:57 -0600

    Add public properties and constructor in LifeArea

--- Model/ValueObjects/LifeArea.cs
+++ Model/ValueObjects/LifeArea.cs
@@ -1,6 +1,14 @@
-﻿namespace Model.ValueObjects;
+﻿using Model.Entities;
+
+namespace Model.ValueObjects;
 
 public class LifeArea
 {
-    
+    public string Name { get; set; }
+    public List<TodoTask>? TodoTasks { get; set; }
+
+    public LifeArea(string name)
+    {
+        Name = name;
+    }
 }
