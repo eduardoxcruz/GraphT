4778fbb3c372b6eb8cb7f43921bf4ef11efe4b8c
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-10-04 23:02:18 -0600

    Add string properties in DatetimeInfo

--- Model/Entities/DatetimeInfo.cs
+++ Model/Entities/DatetimeInfo.cs
@@ -5,4 +5,6 @@ public class DatetimeInfo
     public DateTime? StartTime { get; set; }
     public DateTime? FinishTime { get; set; }
     public DateTime? LimitDate { get; set; }
+    public string? Punctuality { get; set; }
+    public string? TimeSpend { get; set; }
 }
