38ac2760b5587b2e26e412ab5efa92528b83fa73
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-12-28 21:54:22 -0600

    Remove default value from 'complexity' parameter
    The change updates the constructor of TodoTask in Model/Entities/TodoTask.cs, by removing the default value from the 'complexity' parameter. This is to ensure that the complexity level always needs to be explicitly provided when creating a new TodoTask instance.

--- Model/Entities/TodoTask.cs
+++ Model/Entities/TodoTask.cs
@@ -17,7 +17,7 @@ public class TodoTask
     public List<TodoTask> Downstream { get; set; }
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-    public TodoTask(string name, bool isFun, bool isProductive, Status status, Complexity? complexity = null)
+    public TodoTask(string name, bool isFun, bool isProductive, Status status, Complexity? complexity)
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     {
         Name = name;
