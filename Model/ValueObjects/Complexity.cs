513aef0f1d220c443f5bab042d965ecb7d2f58ff
Jose Eduardo Cruz Rosano <eduardoxcruz2000@hotmail.com>
2023-12-28 21:09:30 -0600

    Add new complexity levels to Complexity.cs
    The Complexity class has been updated to include new static complexity levels of Low, Indefinite, and High.

--- Model/ValueObjects/Complexity.cs
+++ Model/ValueObjects/Complexity.cs
@@ -4,5 +4,9 @@ namespace Model.ValueObjects;
 
 public class Complexity : Enumeration
 {
+    public static Complexity Low = new(1, nameof(Low));
+    public static Complexity Indefinite = new(2, nameof(Indefinite));
+    public static Complexity High = new(3, nameof(High));
+    
     public Complexity(int id, string name) : base(id, name) { }
 }
